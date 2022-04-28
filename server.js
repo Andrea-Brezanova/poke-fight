bba6315c682241fce3ca2d0807fb236f4a37c14e
Andrea-Brezanova <andrea.brezan@gmail.com>
2022-04-28 11:57:06 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    console.log("here")
+    res.json({message: "Error"})
+})
+
+app.get('/', (req,res) => {
+    
+})
+
+app.get('/', (req,res) => {
+    
+})
+
+app.get('/', (req,res) => {
+    
+})
+
+
+app.listen(3000)
